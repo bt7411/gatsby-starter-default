c3b4c1c1ca11c4a453c5f4c3be07ee50f0c28117
bt7411 <bt7411@gmail.com>
2020-06-05 20:35:59 +0700

    dbl span

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -10,7 +10,7 @@ const SecondPage = () => (
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2 </p>
-    <Link to="/">Homepage  </Link><span>       </span>
+    <Link to="/">Homepage  </Link><span>                            </span><span>                            </span>
     <Link to="/page-3/">Page 3</Link>
   </Layout>
 )
