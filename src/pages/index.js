d5aaa11789324263271fb5c4e56d23fbb12cb3f6
bt7411 <bt7411@gmail.com>
2020-06-05 14:20:25 +0700

    renamed

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people t1 1409</h1>
+    <h1>Hi people t2 renamed 1418</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
