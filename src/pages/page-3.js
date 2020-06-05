3bb5afaf8dd3ddaa5f67ea23e50782e45480b5c2
bt7411 <bt7411@gmail.com>
2020-06-05 19:58:12 +0700

    converted page 3 form tsx to js

--- src/pages/page-3.js
+++ src/pages/page-3.js
@@ -5,12 +5,12 @@ import { PageProps, Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const ThirdPage = (props: PageProps) => (
+const ThirdPage = () => (
   <Layout>
     <SEO title="Page three" />
     <h1>Hi from the third page</h1>
     <p>Welcome to page 3 </p>
-    <Link to="/">Homepage </Link><span>       </span>
+    <Link to="/">Homepage </Link><span>           </span>
     <Link to="/page-2/">Page 2</Link>
   </Layout>
 )
